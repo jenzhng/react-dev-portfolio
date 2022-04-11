7544ab5e61f24d17212f741f89df71f80503f94c
jenzhng <zhongjenn@gmail.com>
2022-04-11 17:47:42 -0400

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { Icon } from "@iconify/react";
+import nodejsIcon from "@iconify/icons-logos/nodejs-icon";
+import reactIcon from "@iconify/icons-logos/react";
+import rubyIcon from "@iconify/icons-logos/ruby";
+
+class About extends Component {
+  render() {
+    if (this.props.sharedBasicInfo) {
+      var profilepic = "images/" + this.props.sharedBasicInfo.image;
+    }
+    if (this.props.resumeBasicInfo) {
+      var sectionName = this.props.resumeBasicInfo.section_name.about;
+      var hello = this.props.resumeBasicInfo.description_header;
+      var about = this.props.resumeBasicInfo.description;
+    }
+
+    return (
+      <section id="about">
+        <div className="col-md-12">
+          <h1 style={{ color: "black" }}>
+            <span>{sectionName}</span> 
+          </h1>
+          <div className="row center mx-auto mb-5">
+            <div className="col-md-4 mb-5 center">
+              <div className="polaroid">
+                <span style={{ cursor: "auto" }}>
+                  <img
+                    height="250px"
+                    src={profilepic}
+                    alt="Avatar placeholder"
+                  />
+                  <Icon
+                    icon={nodejsIcon}
+                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                  />
+                  <Icon
+                    icon={reactIcon}
+                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                  />
+                  <Icon
+                    icon={rubyIcon}
+                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                  />
+                </span>
+              </div>
+            </div>
+
+            <div className="col-md-8 center">
+              <div className="col-md-10" >
+                <div className="card" >
+                  
+                  <div
+                    className="card-body font-trebuchet text-justify ml-3 mr-3"
+                    style={{
+                      height: "auto",
+                      fontSize: "200%",
+                      lineHeight: "200%",
+					  
+                    }}
+                  >
+                   
+                    {about}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+
+/* <span>{sectionName}</span> 
+*/
+
+export default About;
